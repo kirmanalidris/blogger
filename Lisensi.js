f2dcf104b5c8e3f268933d576965305e32244685
kirmanalidris <56144888+kirmanalidris@users.noreply.github.com>
2020-07-11 17:47:12 +0700

    Update Lisensi.js

--- Lisensi.js
+++ Lisensi.js
@@ -11,12 +11,17 @@
   document.write('  </a>\n');
   document.write('</div>\n');
 
-
-  //ambil nama domain tanpa https contoh: www.kirmanalidris.com
-  var domain = window.location.host;
+  //Verifikasi domain demo-iosstyle
+  var domain = window.location.host;  
   var verifikasi_domain = "demo-iosstyle.blogspot.com";  
+  document.write("<style>#license_page a{border:1px solid #fff; border-radius:100px; padding:7px 15px;}#license_page i{font-size:61px;}#license_page{text-align: center;}#license_page{color:#fff;z-index:10000;padding:80px 40px;position:fixed;top:0;left:0;bottom:0;right:0;background:#000000b3;}</style>");
+  var url = domain;
+  if (url.match(verifikasi_domain)) {
+    document.getElementById("license_page").style.display = "none";
+  }
+  //ambil nama domain tanpa https contoh: www.kirmanalidris.com
+  var domain = window.location.host;  
   var verifikasi_domain = "#demo-iosstyle.blogspot.com";  
-
   document.write("<style>#license_page a{border:1px solid #fff; border-radius:100px; padding:7px 15px;}#license_page i{font-size:61px;}#license_page{text-align: center;}#license_page{color:#fff;z-index:10000;padding:80px 40px;position:fixed;top:0;left:0;bottom:0;right:0;background:#000000b3;}</style>");
   var url = domain;
   if (url.match(verifikasi_domain)) {
